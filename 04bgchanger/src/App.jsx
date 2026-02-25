ccdde85d13c3cdb76b84c6d593b4d7cde27e5d9a
dhrruv013 <dhruvprajapati013@gmail.com>
2026-02-25 18:52:35 +0530

    bgchanger

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -5,38 +5,66 @@ import "./App.css";
 
 function App() {
   const [color, setColor] = useState("olive");
-
+  // setColor = () => {
+  //   {
+  //     backgroundColor: "green";
+  //   }
+  // };
   return (
-    <div
-      className="w-full h-screen duration-200"
-      style={{ backgroundColor: color }}
-    >
-      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+    <>
+      <div
+        className="w-screen h-screen duration-200 flex justify-center items-end "
+        style={{ backgroundColor: color }}
+      >
+        <div
+          className="w-fit mb-20 p-4  flex gap-6 rounded-2xl"
+          style={{ backgroundColor: "white" }}
+        >
           <button
-            onClick={() => setColor("red")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
-            style={{ backgroundColor: "red" }}
-          >
-            Red
-          </button>
-          <button
-            onClick={() => setColor("green")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            className="rounded-2xl p-3 text-white"
             style={{ backgroundColor: "green" }}
+            onClick={() => setColor("green")}
           >
             Green
           </button>
           <button
-            onClick={() => setColor("blue")}
-            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            className="rounded-2xl p-3 text-white"
+            style={{ backgroundColor: "red" }}
+            onClick={() => setColor("red")}
+          >
+            Red
+          </button>
+          <button
+            className="rounded-2xl p-3 text-black"
+            style={{ backgroundColor: "yellow" }}
+            onClick={() => setColor("yellow")}
+          >
+            Yellow
+          </button>
+          <button
+            className="rounded-2xl p-3 text-black"
+            style={{ backgroundColor: "orange" }}
+            onClick={() => setColor("orange")}
+          >
+            Orange
+          </button>
+          <button
+            className="rounded-2xl p-3 text-white"
             style={{ backgroundColor: "blue" }}
+            onClick={() => setColor("blue")}
           >
             Blue
           </button>
+          <button
+            className="rounded-2xl p-3 text-white"
+            style={{ backgroundColor: "black" }}
+            onClick={() => setColor("black")}
+          >
+            Black
+          </button>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
